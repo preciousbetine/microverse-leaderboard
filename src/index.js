bdf7623e4ea43167ef4fec8d53a9cf4d84f2bce3
Precious Betine <preciousbetine@gmail.com>
2023-03-08 08:03:23 +0100

    Hit the API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './style.scss';
+import { addScore, showScores } from './modules/game';
+import showToast from './modules/toast';
+
+const refreshButton = document.getElementById('refresh');
+refreshButton.addEventListener('click', async () => {
+  showScores();
+});
+
+const submitButton = document.querySelector('form > button');
+submitButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const user = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+  const newScore = await addScore(user, score);
+  document.querySelector('form').reset();
+  showToast(newScore.result);
+});
+
+showScores();
---
+++ src/modules/game.js
@@ -0,0 +1,38 @@
+// const gameID = '4oNvU3Bhjf2ub7nbIBB5';
+const gameID = 'xLw0Vz72706E0aKqRFgf';
+const apiBase = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+
+const getScores = async (e) => {
+  const games = await fetch(`${apiBase}/games/${gameID}/scores/`);
+  const gamesJSON = await games.json();
+  return gamesJSON.result;
+};
+
+const addScore = async (user, score) => {
+  const newScore = await fetch(`${apiBase}/games/${gameID}/scores/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ user, score }),
+  });
+  const newScoreJSON = await newScore.json();
+  return newScoreJSON;
+};
+
+const showScores = async () => {
+  const scoresList = document.getElementById('scores');
+  scoresList.innerHTML = '';
+  const scores = await getScores();
+
+  scores.forEach((score) => {
+    const li = document.createElement('li');
+    li.innerHTML = `${score.user}: ${score.score}`;
+    scoresList.appendChild(li);
+  });
+};
+
+module.exports = {
+  addScore,
+  showScores,
+};
